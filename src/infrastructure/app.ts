a4b43dd8ac067d3638aa9e8f2d313db6a39337d2
Lucie-Lenovo-Portable <Lucie.Ferrero.Pro@gmail.com>
2026-03-30 10:36:15 +0200

    Stadiums Handler

--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -3,9 +3,10 @@ import { Hono } from 'hono'
 import { matchsRouter } from './routes/matchs'
 import { teamsRouter } from './routes/teams'
 import { homeRouter } from './routes/home'
-
+import { stadiumsRouter } from './routes/stadiums'
 export const app = new Hono()
 
 app.route('/matchs', matchsRouter);
 app.route('/teams', teamsRouter);
+app.route('/stadiums', stadiumsRouter);
 app.route('', homeRouter);
---
+++ src/infrastructure/handlers/stadium/GetStadiumsHandler.ts
@@ -0,0 +1,43 @@
+import { Context } from "hono";
+import {stadiums} from "../../mock/stadiums";
+import { city } from "infrastructure/mock/cities";
+import { $ } from "bun";
+
+export class GetStadiumsHandler {
+    async handle(c: Context) {
+        const cityName = c.req.query('city[name]') || "";
+        const countryCode = c.req.query('country[code]') || "";
+        const counstryName = c.req.query('country[name]') || "";
+        if (cityName) {
+            const filteredStadiums = stadiums.filter(s => s.city.name.toLowerCase().includes(cityName.toLowerCase()));
+            return c.json({
+                success: true,
+                message: `Stadiums filtered by city[name]:`,
+                cityName : cityName,
+                data: filteredStadiums
+            });
+        }
+        if (countryCode) {
+            const filteredStadiums = stadiums.filter(s => s.city.country.code.toLowerCase().includes(countryCode.toLowerCase()));
+            return c.json({
+                success: true,
+                message: `Stadiums filtered by country[code]:`,
+                data: filteredStadiums
+            });
+        }
+        if (counstryName) {
+            const filteredStadiums = stadiums.filter(s => s.city.country.name.toLowerCase().includes(counstryName.toLowerCase()));
+            return c.json({
+                success: true,
+                message: `Stadiums filtered by country[name]:`,
+                data: filteredStadiums
+            });
+        }
+        return c.json({
+            success: true,
+            message : 'All stadiums',
+            data : stadiums
+        }, 200);
+    }
+}
+
